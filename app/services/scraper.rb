6fb7175cecce46d3fa0c4a75036d99d890587480
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-11-15 16:47:34 +0000

    make a get request using httparty

--- app/services/scraper.rb
+++ app/services/scraper.rb
@@ -1,13 +1,24 @@
-require "open-uri"
-require "net/http"
+## Using the net/http method
+# require "open-uri"
+# require "net/http"
 
-url = "https://en.wikipedia.org/wiki/Douglas_Adams"
+# url = "https://en.wikipedia.org/wiki/Douglas_Adams"
 
-# Convert URL string into a URI object using the open-uri gem
-uri = URI.parse(url)
-# Pass URI object to the get_response method which provides a Net::HTTPResponse object
-response = Net::HTTP.get_response(uri)
-# Use the Net::HTTPResponse ojbect's body method to provide the entire HTML document
+# # Convert URL string into a URI object using the open-uri gem
+# uri = URI.parse(url)
+# # Pass URI object to the get_response method which provides a Net::HTTPResponse object
+# response = Net::HTTP.get_response(uri)
+# # Use the Net::HTTPResponse ojbect's body method to provide the entire HTML document
+# html = response.body
+# # print out the html
+# puts html
+
+## Using the httparty method
+require "httparty"
+
+# Make a GET request using httparty gem
+response = HTTParty.get("https://en.wikipedia.org/wiki/Douglas_Adams")
+
+# Use the HTTParty::Response ojbect's body method to provide the entire HTML document
 html = response.body
-# print out the html
 puts html
